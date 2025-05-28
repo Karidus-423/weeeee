dcda2ab80c0588eb596a3846db0f83570b869574
Karidus-423 <kapuerto23@gmail.com>
2025-05-28 01:15:41 +0000

    basics

--- widget/Bar.lua
+++ widget/Bar.lua
@@ -7,32 +7,8 @@ local Mpris = astal.require("AstalMpris")
 local Battery = astal.require("AstalBattery")
 local Wp = astal.require("AstalWp")
 local Network = astal.require("AstalNetwork")
-local Tray = astal.require("AstalTray")
 local Hyprland = astal.require("AstalHyprland")
 local map = require("lib").map
-
-local function SysTray()
-	local tray = Tray.get_default()
-
-	return Widget.Box({
-		class_name = "SysTray",
-		bind(tray, "items"):as(function(items)
-			return map(items, function(item)
-				return Widget.MenuButton({
-					tooltip_markup = bind(item, "tooltip_markup"),
-					use_popover = false,
-					menu_model = bind(item, "menu-model"),
-					action_group = bind(item, "action-group"):as(
-						function(ag) return { "dbusmenu", ag } end
-					),
-					Widget.Icon({
-						gicon = bind(item, "gicon"),
-					}),
-				})
-			end)
-		end),
-	})
-end
 
 local function Wifi()
 	local network = Network.get_default()
@@ -167,10 +143,10 @@ return function(gdkmonitor)
 		Widget.CenterBox({
 			Widget.Box({
 				halign = "START",
-				Workspaces(),
+				Media(),
 			}),
 			Widget.Box({
-				Media(),
+				Workspaces(),
 			}),
 			Widget.Box({
 				halign = "END",
